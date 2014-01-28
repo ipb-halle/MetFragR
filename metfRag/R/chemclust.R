ff43a8d35a574fb20037db0555f090c7eeaf3c51
Steffen Neumann <sneumann@ipb-halle.de>
2014-01-28 09:00:12 +0100

    Clustering now with main title, options passed to dendromat()

--- metfRag/R/chemclust.R
+++ metfRag/R/chemclust.R
@@ -9,18 +9,90 @@
 ## DataFrames with SMILEs/InChIs and optionally scores.
 ##
 
+#' Chemical Clustering of MetFrag and MetFusion results SDF files
+#' 
+#' Cluster a set of compounds ranked with MetFrag and MetFusion
+#' 
+#' @aliases plotMetClust
+#' @usage plotMetClust(mols, sdf, hclust, scoreprop="Score", idprop="DatabaseID")
+#' @param hclust An object of class hclust which describes the tree produced by a prior clustering process. 
+#' @param mols The a list of rCDK \code{mols}. 
+#' @param smiles A character vector of SMILES codes. 
+#' @param filename A filename to an SD File. If boths \code{mols} 
+#' and \code{filename} are given, filename is ignored. Files are read by \code{cdk::load.molecules()}
+#' @param type If a \code{filename} was given,  the \code{type} will be passed to \code{cdk::load.molecules()}. 
+#' The default value \code{auto} uses the filename extension.
+#' @param scoreprop The name of the property of the molecules where the score is kept
+#' @param idprop The name of the property of the molecules where the database ID is kept
+#' @import squash
+#' @importFrom tools file_ext
+#' @examples 
+#'        smiles <- c('CCC', 'CCN', 'CCN(C)(C)',
+#'                    'c1ccccc1Cc1ccccc1',
+#'                    'C1CCC1CC(CN(C)(C))CC(=O)CC')
+#'        mols <- parse.smiles(smiles)
+#'        dummy <- mapply(set.property, mols, "Score", c(1,2,3,4,5))
+#'        dummy <- mapply(set.property, mols, "DatabaseID", c("C1", "C2", "C3", "C4", "C5"))
+#'        plotMetClust(mols)
+#' 
+#' @export
 
+plotMetClust <- function(mols=NULL, filename=NULL, type=c("auto", "sdf", "smi"), 
+                         hclust=NULL, scoreprop="Score", idprop="DatabaseID")
+{
+  
+}
 
-getscores <- function(x, scoreprop="Score"){
+hclust.mols <- function(mols=NULL, smiles=NULL, filename=NULL,
+                        scoreprop="Score", idprop="DatabaseID") 
+{
+
+  if (!missing(mols)) {
+    ## Placeholder
+    ## Nothing tbd.
+  } else if (!missing(smiles)) {
+    mols <- parse.smiles(smiles)    
+  } else if (!missing(filename)) {
+    mols <- load.molecules(filename)
+  }  else {
+    stop("no compounds provided")
+  }
+
+  ## Prepare compounds
+  invisible(sapply(mols, do.typing))
+  invisible(sapply(mols, do.aromaticity))    
+
+  
+  ## Calculate fingerprints and calculate dists
+  fps <- lapply(mols, get.fingerprint, type = "extended")
+  fp.sim <- fp.sim.matrix(fps, method = "tanimoto")
+  fp.dist <- 1 - fp.sim
+  cluster  <- hclust(as.dist(fp.dist))
+
+  # Put additional (chemical) information into hclust object
+  cluster$mols <- mols
+  cluster$labels <- sapply(mols, get.title)
+  cluster$scores <- getScores(mols, scoreprop)
+  cluster$ids <- getDatabaseIDs(mols, idprop)
+  
+  return(cluster)
+}  
+
+# h1 <- hclust.mols(filename="/vol/R/rguha/cdkr/data/BL.sdf")
+# h2 <- hclust.mols(mols=NULL, filename=NULL, scoreprop="Score", idprop="DatabaseID")
+# h3 <- hclust.mols(mols=NULL, filename=NULL, scoreprop="Score", idprop="DatabaseID")
+
+getScores <- function(x, scoreprop="Score"){
   return (max(sapply(x, function(x) as.numeric(get.property(x, scoreprop)))))
 }
 
 getDatabaseIDs <- function(x, idprop="DatabaseID") {
-  return (sapply(1:length(x), function(id) {
-    if (get.property(x[[id]], idprop) == 'NA')
-      return (id)
+  return (sapply(1:length(x), function(i) {
+    id <- get.property(x[[i]], idprop)
+    if (is.na(id) || id == 'NA')
+      return (i)
     else
-      return (get.property(x[[id]], idprop))
+      return (get.property(x[[i]], idprop))
   }))
 }
 
@@ -31,19 +103,23 @@ getDatabaseIDs <- function(x, idprop="DatabaseID") {
 #' 
 #' @aliases plotCluster
 #' @usage plotCluster(mols, scoreprop="Score", idprop="DatabaseID")
-#' @param w The a list of rCDK \code{mols}
+#' @param mols The a list of rCDK \code{mols}
 #' @param scoreprop The name of the property of the molecules where the score is kept
 #' @param idprop The name of the property of the molecules where the database ID is kept
+#' @param ... the remaining parameters are passed down to \code{dendromat()}
+#' @import squash
 #' @examples 
 #'        smiles <- c('CCC', 'CCN', 'CCN(C)(C)',
 #'                    'c1ccccc1Cc1ccccc1',
 #'                    'C1CCC1CC(CN(C)(C))CC(=O)CC')
 #'        mols <- parse.smiles(smiles)
-#'              plotCluster(mols)
+#'        dummy <- mapply(set.property, mols, "Score", c(1,2,3,4,5))
+#'        dummy <- mapply(set.property, mols, "DatabaseID", c("C1", "C2", "C3", "C4", "C5"))
+#'        plotCluster(mols)
 #' 
 #' @export
 
-plotCluster <- function(mols, scoreprop="Score", idprop="DatabaseID") {
+plotCluster <- function(mols, scoreprop="Score", idprop="DatabaseID", ...) {
   
   clusters  <- list()
   
@@ -69,9 +145,11 @@ plotCluster <- function(mols, scoreprop="Score", idprop="DatabaseID") {
   fp.sim <- fp.sim.matrix(fps, method = "tanimoto")
   fp.dist <- 1 - fp.sim
   cluster  <- hclust(as.dist(fp.dist))
+
+  ## prepare for plotting
   classlabel   <- as.factor(paste(getDatabaseIDs(mols, idprop),
                                   cutree(cluster, h=0.2), sep=" "))
-  clusterscore <- tapply(mols, classlabel, getscores)
+  clusterscore <- tapply(mols, classlabel, getScores)
   score.cmap <- makecmap(score.explained,
                          n = 8,
                          colFn = colorRampPalette(c("black","red")))
@@ -82,12 +160,26 @@ plotCluster <- function(mols, scoreprop="Score", idprop="DatabaseID") {
   
   
   dendromat(cluster, cluster.mat,
-            ylab = 'Tanimoto distance', main = "")
+            ylab = 'Tanimoto distance', ...)
   
-  legend(x=0.96,y=0.7,
-         legend=c("0.6","0.65","0.7","0.75","0.8","0.85","0.9","0.95"),
+  legend(x=0.92,y=0.7,
+         legend=score.cmap$breaks[-1], # exclude.lowest
          fill=score.cmap$colors,
          title="Score >",
          xpd=NA, bty = "n")
   
+}
+
+
+mcss <- function(x) {
+  l <- length(x)
+  
+  if (l==1)
+    return (x[[1]])
+  
+  consensus <- x[[1]]  
+  for (i in 2:l) {
+    consensus <- get.mcs(consensus, x[[i]])
+  }
+  return (consensus)  
 }
