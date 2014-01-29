a3ed70391a3d996b6e194acd257a4e2dd9b288e9
Steffen Neumann <sneumann@ipb-halle.de>
2014-01-29 23:45:09 +0100

    First go at plotting MCSS into dendrogram

--- metfRag/R/chemclust.R
+++ metfRag/R/chemclust.R
@@ -86,6 +86,11 @@ getScores <- function(x, scoreprop="Score"){
   return (max(sapply(x, function(x) as.numeric(get.property(x, scoreprop)))))
 }
 
+getPeaksExplained <- function(x){
+  return (max(sapply(x, 
+                     function(x) as.numeric(get.property(x, "NoPeaksExplained")))))
+}
+
 getDatabaseIDs <- function(x, idprop="DatabaseID") {
   return (sapply(1:length(x), function(i) {
     id <- get.property(x[[i]], idprop)
@@ -171,15 +176,195 @@ plotCluster <- function(mols, scoreprop="Score", idprop="DatabaseID", ...) {
 }
 
 
-mcss <- function(x) {
-  l <- length(x)
+#' Chemical Clustering
+#' 
+#' Calculate the Maximum Common Substructure
+#' 
+#' @aliases getMCSS
+#' @usage getMCSS(mols)
+#' @param mols The a list of rCDK \code{mols}
+#' @examples 
+#'        smiles <- c('CCC', 'CCN', 'CCN(C)(C)',
+#'                    'c1ccccc1Cc1ccccc1',
+#'                    'C1CCC1CC(CN(C)(C))CC(=O)CC')
+#'        mols <- parse.smiles(smiles)
+#'        dummy <- getMCSS(mols)
+#' 
+#' @export
+
+getMCSS <- function(mols) {
+  l <- length(mols)
+  if (l<1)
+    stop("No MCSS for less than one molecule!")
   
   if (l==1)
-    return (x[[1]])
+    return (mols[[1]])
   
-  consensus <- x[[1]]  
+  consensus <- mols[[1]]  
   for (i in 2:l) {
-    consensus <- get.mcs(consensus, x[[i]])
+    consensus <- get.mcs(consensus, mols[[i]])
   }
   return (consensus)  
 }
+
+#' Chemical Clustering
+#' 
+#' Calculate the Maximum Common Substructure
+#' 
+#' @aliases getMCSS
+#' @usage getMCSS(mols)
+#' @param hcluster Previously created hclust for \code{mols}
+#' @param mols The a list of rCDK \code{mols}
+#' @param h dendrogram cutoff
+#' #' @examples 
+#'        smiles <- c('CCC', 'CCN', 'CCN(C)(C)',
+#'                    'c1ccccc1Cc1ccccc1',
+#'                    'C1CCC1CC(CN(C)(C))CC(=O)CC')
+#'        mols <- parse.smiles(smiles)
+#'        cluster <- metfRag:::hclust.mols(mols)
+#'        clusterreps <- getMCSS(mols, cluster) 
+#' 
+#' @export
+
+getClusterMCSS <- function(hcluster, mols, h=0.2) {
+    classlabel   <- as.factor(cutree(hcluster, h=h))
+    clusterreps  <- tapply(mols, classlabel, metfRag:::getMCSS)
+    clusterreps
+}
+  
+
+#' Chemical Clustering
+#' 
+#' Calculate the Maximum Common Substructure
+#' Modified from stats::rect.hclust()
+#' 
+#' @aliases myimages.hclust
+#' @usage myimages.hclust (tree, k = NULL, which = NULL, x = NULL, 
+#' h = NULL, images = NULL, cluster = NULL, mols=NULL) 
+#' @param tree an object of the type produced by \code{hclust},
+#' that was just plot()ed, and where the images should be overlaid
+#' @param k,h Scalar. Cut the dendrogram such that either exactly
+#' \code{k} clusters are produced or by cutting at height \code{h}.
+#' (either k or h needs to be specified)
+#' @param which,x A vector selecting the clusters around which a
+#' rectangle should be drawn. \code{which} selects clusters by number
+#' (from left to right in the tree), \code{x} selects clusters
+#' containing the respective horizontal coordinates. Default is
+#' \code{which = 1:k} (either x or which needs to be specified)
+#' @param images
+#' @param cluster Optional vector with cluster memberships as returned by
+#' \code{cutree(hclust.obj, k = k)}, can be specified for efficiency if
+#' already computed.
+#' @param border Vector with border colors for the rectangles, NULL for none.
+#' @param mols The a list of rCDK \code{mols}
+#' 
+#' @export
+
+
+myimages.hclust <- function (tree, k = NULL, which = NULL, x = NULL, h = NULL,
+                             images = NULL, cluster = NULL, mols=NULL, border = NULL) 
+{
+  if (length(h) > 1L | length(k) > 1L) 
+    stop("'k' and 'h' must be a scalar")
+
+  if (!is.null(h)) {
+    if (!is.null(k)) 
+      stop("specify exactly one of 'k' and 'h'")
+    k <- min(which(rev(tree$height) < h))
+    k <- max(k, 2)
+  } else if (is.null(k)) {
+    stop("specify exactly one of 'k' and 'h'")
+  }
+  
+  if (k < 2 | k > length(tree$height)) {
+    stop(gettextf("k must be between 2 and %d", length(tree$height)), 
+         domain = NA)
+  }
+  
+  if (is.null(cluster)) {
+    cluster <- cutree(tree, k = k)
+  }
+  
+  ## cutree returns classes sorted by data, we need classes
+  ## as occurring in the tree (from left to right)  
+  clustab <- table(cluster)[unique(cluster[tree$order])]
+  m <- c(0, cumsum(clustab))
+  if (!is.null(x)) {
+    if (!is.null(which)) 
+      stop("specify exactly one of 'which' and 'x'")
+    which <- x
+    for (n in seq_along(x)) which[n] <- max(which(m < x[n]))
+  } else if (is.null(which)) {
+    which <- 1L:k
+  }
+  
+  if (any(which > k)) 
+    stop(gettextf("all elements of 'which' must be between 1 and %d", 
+                  k), domain = NA)
+  
+  retval <- list()
+  if (!is.null(border)) {
+    border <- rep_len(border, length(which))
+    for(n in seq_along(which)) {
+      rect(m[which[n]]+0.66, par("usr")[3L],
+           m[which[n]+1]+0.33, mean(rev(tree$height)[(k-1):k]),
+           border = border[n])
+      retval[[n]] <- which(cluster==as.integer(names(clustab)[which[n]]))
+    }
+  }
+  
+  retval <- list()
+  for (n in seq_along(which)) {
+    consensus <- getMCSS(mols[which(cluster == as.integer(names(clustab)[which[n]]))])
+    xwidth <- par("usr")[2]-par("usr")[1]
+    ywidth <- par("usr")[4]-par("usr")[3]
+    aspect <- xwidth/ywidth
+    
+    xleft <- m[which[n]] + 0.66
+    width <- m[which[n] + 1] + 0.33 - xleft
+    ybottom <- par("usr")[3L]
+    
+    rasterImage(view.image.2d(consensus),
+                xleft, 
+                ybottom,
+                xleft + width,        
+                (ybottom + width)/aspect)
+    
+    retval[[n]] <- which(cluster == as.integer(names(clustab)[which[n]]))
+  }
+  invisible(retval)
+}
+
+
+myidentify <- function (x, FUN = NULL, N = 20, MAXCLUSTER = 20, DEV.FUN = NULL, ...) 
+{
+  cluster <- cutree(x, k = 2:MAXCLUSTER)
+  retval <- list()
+  oldk <- NULL
+  oldx <- NULL
+  DEV.x <- grDevices::dev.cur()
+  for (n in 1L:N) {
+    grDevices::dev.set(DEV.x)
+    X <- locator(1)
+    if (is.null(X)) 
+      break
+    k <- min(which(rev(x$height) < X$y), MAXCLUSTER)
+    k <- max(k, 2)
+    retval[[n]] <- unlist(myimages.hclust(x, k = k, x = X$x, 
+                                          cluster = cluster[, k - 1]))
+    if (!is.null(FUN)) {
+      if (!is.null(DEV.FUN)) {
+        grDevices::dev.set(DEV.FUN)
+      }
+      retval[[n]] <- FUN(retval[[n]], ...)
+    }
+    oldx <- X$x
+    oldk <- k
+  }
+  grDevices::dev.set(DEV.x)
+  invisible(retval)
+}
+
+
+# plot(cluster)
+# myidentify(cluster)
