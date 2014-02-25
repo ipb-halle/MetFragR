1aeb673c0e4813ad25c9efcc7857661bd114a8ee
cruttkie <cruttkie@ender.ipb-sub.ipb-halle.de>
2014-02-25 16:12:18 +0100

    replaced rep_len by rep

--- metfRag/R/chemclust.R
+++ metfRag/R/chemclust.R
@@ -272,7 +272,7 @@ myimages.hclust <- function (tree, k = NULL, which = NULL, x = NULL, h = NULL,
   
   retval <- list()
   if (!is.null(border)) {
-    border <- rep_len(border, length(which))
+    border <- rep(border, length_out = length(which))
     for(n in seq_along(which)) {
       rect(m[which[n]]+0.66, par("usr")[3L],
            m[which[n]+1]+0.33, mean(rev(tree$height)[(k-1):k]),
@@ -368,7 +368,7 @@ myimages.clustNumbers <- function (tree, k = NULL, which = NULL, x = NULL, h = N
   
   retval <- list()
   if (!is.null(border)) {
-    border <- rep_len(border, length(which))
+    border <- rep(border, length_out = length(which))
     for(n in seq_along(which)) {    
       rect(m[which[n]]+0.66, par("usr")[3L],
            m[which[n]+1]+0.33, 
