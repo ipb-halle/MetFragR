f2a86fd52fb8f6a8bb084baf3f0b510578c8df81
AK2083 <ankh.egypt.public@googlemail.com>
2014-04-03 10:57:02 +0200

    fixed some bugs in database access
    Signed-off-by: AK2083 <ankh.egypt.public@googlemail.com>

--- metfRag/R/database.R
+++ metfRag/R/database.R
@@ -17,7 +17,7 @@ db.search <- function(file, db, type, value)
 #KEGG
 db.kegg.getId <- function(type, value)
 {
-  kegg.types  <- c('exact_mass', 'entry', 'formula');
+  kegg.types  <- c('exact_mass', 'mol_weight', 'formula');
   kegg.loc    <- "http://rest.kegg.jp/find";
   kegg.db     <- "compound";  
   
@@ -37,41 +37,54 @@ db.kegg.getId <- function(type, value)
 
 db.kegg.convertString <- function(idstring)
 {
-  ids   <- str_split(idstring, "\n");
+  ids   <- strsplit(idstring, "\n");
   size  <- length(ids[[1]]);
-  kegg.id   <- NULL;  
-  converted <- sapply(ids[[1]],str_split,"\t")
+  kegg.id   <- NULL;
+  tmp       <- NULL;
+  converted <- sapply(ids[[1]],strsplit,"\t")
   
-  for (i in 1:(size-1))
+  for (i in 1:(size))
   {
-    kegg.id <- rbind(kegg.id, converted[[i]][1]);
+    tmp <- strsplit(converted[[i]][1],":");
+    kegg.id <- rbind(kegg.id, tmp[[1]][2]);
   }
   
   colnames(kegg.id) <- 'CPD';
   return(kegg.id);
 }
   
-db.kegg.MoleculeToFile <- function(file, ids)
+db.kegg.getMoleculeContainer <- function(file=NULL, ids)
 {
   if (length(ids) == 0)
   { return(FALSE); }  
+  
+  ids <- paste("cpd:",ids,sep="");
   
   kegg.loc  <- "http://rest.kegg.jp";
   kegg.op   <- 'get';
   kegg.filetype <- 'mol';
   kegg.id <- paste(ids, collapse="+");        
   
-  url     <- paste(kegg.loc,kegg.op,kegg.id,kegg.filetype,sep="/"); 
-  download.file(url, file);
+  kegg.url     <- paste(kegg.loc,kegg.op,kegg.id,kegg.filetype,sep="/"); 
   
-  if (file.exists(file))
-  { return(TRUE); }
+  if (missing(file) == TRUE)
+  {
+    if (url.exists(kegg.url) == TRUE)
+    { return(load.molecules(kegg.url)); }    
+  }
+  else
+  {
+    download.file(url, file);
+    
+    if (file.exists(file))
+    { return(TRUE); }    
+  }
   
   return(FALSE);
 }
 
 #PubChem
-db.pubchem.getId <- function(type, value=NULL)
+db.pubchem.getId <- function(type=NULL, value=NULL)
 {
   pc.loc <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?";
   pc.query <- "db=pccompound&term=";
@@ -80,22 +93,27 @@ db.pubchem.getId <- function(type, value=NULL)
   if (any(pc.type == type) == TRUE && missing(value) != TRUE)
   {
     if (type == pc.type[2] || type == pc.type[3])
-    { value = as.double(value); }
-    
-    if (type != pc.type[1])
     {
+      value <- as.double(value);
       pc.mass <- paste(100,
                        "[",toupper(type),"]:",
                        value,
                        "[",toupper(type),"]",
                        sep="");
     }
+    else if (type == pc.type[4])
+    {
+      pc.mass <- paste("[",toupper(type),"]",
+                       value,
+                       "[",toupper(type),"]",
+                       sep="");      
+    }
     else
     { pc.mass <- value; }
     
     pc.url <- paste(pc.loc, pc.query, pc.mass, sep="");
-    pc.data <- getURI(pc.url);
     
+    pc.data <- getURI(pc.url);
     return(db.pubchem.XMLToList(pc.data));
   }
   
@@ -105,14 +123,20 @@ db.pubchem.getId <- function(type, value=NULL)
 db.pubchem.XMLToList <- function(xml)
 {
   pc.data <- xmlToList(xmlParse(xml));
-  pc.data <- as.matrix(pc.data$IdList);
-  colnames(pc.data) <- 'CID';
-  rownames(pc.data) <- NULL;
   
-  return(pc.data);
+  if (is.null(pc.data$WarningList) == TRUE)
+  { 
+    pc.data <- as.matrix(pc.data$IdList);
+    colnames(pc.data) <- 'CID';
+    rownames(pc.data) <- NULL;
+    
+    return(pc.data);  
+  }
+  
+  return(NULL);
 }
 
-db.pubchem.MoleculeToFile <- function(file, ids)
+db.pubchem.getMoleculeContainer <- function(file=NULL, ids)
 {
   pc.loc      <- "http://pubchem.ncbi.nlm.nih.gov/rest/pug";
   pc.db       <- "compound"
@@ -121,10 +145,21 @@ db.pubchem.MoleculeToFile <- function(file, ids)
   pc.ids <- paste(ids,collapse=",");
   
   pc.url <- paste(pc.loc,pc.db,pc.type,pc.ids,pc.filetype,sep="/");
-  download.file(pc.url, file);
   
-  if (file.exists(file))
-  { return(TRUE); }  
+  if (missing(file) == TRUE)
+  {
+    if (url.exists(pc.url) == TRUE)
+    {
+      return(load.molecules(pc.url));
+    }    
+  }
+  else
+  {
+    download.file(pc.url, file);
+    
+    if (file.exists(file))
+    { return(TRUE); }    
+  }  
   
   return(FALSE);
 }
